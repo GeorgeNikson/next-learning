7226e15f38b563f780c16dee2e1cbc1d903b8fe9
GeorgeNikson <georgiynikwow@gmail.com>
2021-06-20 19:44:46 +0300

    json server + concurrently for running dev + serv

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,11 +1,20 @@
-import Link from "next/link"
+import Link from "next/link";
+import Head from "next/head";
 import classes from "../../styles/error.module.scss";
 
 export default function ErrorPage() {
-    return (
-        <>
-            <h1 className={classes.error}>Error 404</h1>
-            <p>Перейти на <Link href="/"><a>главную страницу</a></Link></p>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Ошибка!</title>
+      </Head>
+      <h1 className={classes.error}>Error 404</h1>
+      <p>
+        Перейти на{" "}
+        <Link href="/">
+          <a>главную страницу</a>
+        </Link>
+      </p>
+    </>
+  );
 }
